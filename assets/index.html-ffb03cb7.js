0db08a5a480def7196bf038528dcbaaddb6e7169
annkots <71260515+annkots@users.noreply.github.com>
2025-05-22 15:30:55 +0000

    Deploying to gh-pages from  @ 2c01b453682221ebdbb2d36109ba1f1ff4c68ae1 🚀

---
+++ assets/index.html-ffb03cb7.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-4b1420f2","path":"/els-for-languages/","title":"Endless Lifecycle Support for Languages","lang":"en-US","frontmatter":{},"headers":[]}');export{e as data};
