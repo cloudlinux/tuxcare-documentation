7b3b9109eb7dcf4fc0cf6b2d5080270c9d4b89a3
Nadya2502 <fornmer@yandex.ru>
2026-07-17 13:40:18 +0200

    ELSDOC-349, added ratchet and thruway to php libraries

--- docs/.vuepress/config-client/sidebar.ts
+++ docs/.vuepress/config-client/sidebar.ts
@@ -345,6 +345,10 @@ export default {
                     icon: '/images/php-logo.webp',
                 },
                 {
+                    path: '/els-for-libraries/ratchet/',
+                    icon: '/images/TuxCare_color_icon.webp',
+                },
+                {
                     path: '/els-for-libraries/saloon/',
                     icon: '/images/saloon.webp',
                 },
@@ -355,6 +359,10 @@ export default {
                 {
                     path: '/els-for-libraries/symfony/',
                     icon: '/images/symfony-logo.webp',
+                },
+                {
+                    path: '/els-for-libraries/thruway/',
+                    icon: '/images/TuxCare_color_icon.webp',
                 },
                 {
                     path: '/els-for-libraries/twig/',
